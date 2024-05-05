0293c67586bda3f07a7bd621ecec45fe71e46874
anas <anasanasri3030@gmail.com>
2024-05-05 15:53:34 +0100

    V1 Befor spring security

---
+++ src/main/java/ma/m2t/paywidget/CorsConfig.java
@@ -0,0 +1,17 @@
+package ma.m2t.paywidget;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200") // Allow requests from this origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE"); // Allow specific HTTP methods
+    }
+}
+
